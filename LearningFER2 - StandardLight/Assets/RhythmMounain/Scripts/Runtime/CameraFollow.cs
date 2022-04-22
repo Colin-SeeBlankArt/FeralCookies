ae64b98bc3e08b8153e5c9db2fe0a21d3c28c1fb
Zachary Sheppard <the_ds_guy@yahoo.com>
2022-04-22 05:03:41 -0500

    Fixed several floating point origin issues

--- LearningFER2 - StandardLight/Assets/RhythmMounain/Scripts/Runtime/CameraFollow.cs
+++ LearningFER2 - StandardLight/Assets/RhythmMounain/Scripts/Runtime/CameraFollow.cs
@@ -1,21 +1,51 @@
-
+using Dreamteck.Forever;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
-
-    public float smoothSpeed = 10f;
-
     public Vector3 offset;
 
-    void LateUpdate()
+    public float smoothSpeed = 10f;
+    public Transform target;
+
+    private Vector3? Delta = null;
+
+    private void FloatingOriginOnonOriginOffset(Vector3 delta)
     {
-        Vector3 desiredPostion = target.position + offset; //finds where we want to snap camera too, every frame
-        Vector3 smoothPostion = Vector3.Lerp(transform.position, desiredPostion, smoothSpeed * Time.deltaTime);  //defines how close the camera gets, based on Desired and Smooth, not sure how though
+        transform.position -= delta;
+    }
+
+    private void LateUpdate()
+    {
+        UpdateFollowTransform(1f);
+    }
+
+    private void OnDisable()
+    {
+        FloatingOrigin.onOriginOffset -= FloatingOriginOnonOriginOffset;
+    }
+
+    private void OnEnable()
+    {
+        FloatingOrigin.onOriginOffset += FloatingOriginOnonOriginOffset;
+    }
+
+    private void OnValidate()
+    {
+        UpdateFollowTransform(1f);
+    }
+
+    private void UpdateFollowTransform(float t)
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        var desiredPostion = target.TransformPoint(offset); //finds where we want to snap camera too, every frame
+        var smoothPostion = Vector3.Lerp(transform.position, desiredPostion, t); //defines how close the camera gets, based on Desired and Smooth, not sure how though
         transform.position = smoothPostion;
 
         transform.LookAt(target);
     }
-
 }
